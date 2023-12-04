6184144b833f2da978ff87e80b8a22386c7a1a68
Pangestuy <2110512034@mahasiswa.upnvj.ac.id>
2023-12-04 21:25:30 +0700

    splashcreen done mas

---
+++ app/src/main/java/com/example/fundpal/view/fragment/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.fundpal.view.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.fundpal.R;
+import com.example.fundpal.view.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Initialization of views
+        View decorView = getWindow().getDecorView();
+
+        // Hide the status bar
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Timer for launching MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
